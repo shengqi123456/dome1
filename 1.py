2e26258d4f1f95f4fd5548a87a63050f9a847b1f
陈俊杰 <1742940314.@qq.com>
2020-11-26 21:45:02 +0800

    pycharm 单例模式 装饰器

--- 1.py
+++ 1.py
@@ -1,8 +1,29 @@
-print("123")
-print("2222")
-print("333")
-print("4321")
-print("54321")
-le=[1,2,3,4,5,6]
-le.clear()
-print(le)
+# 装饰器
+# import time
+# def qwe(fn):
+#     def ineer(*args,**kwargs):
+#         s=time.time()
+#         ret=fn(*args,**kwargs)
+#         e=time.time()
+#         print(f"所用的时间是{e-s}")
+#         return ret
+#     return ineer()
+# @qwe
+# def ll():
+#     n=0
+#     for i in range(1000000):
+#         n+=i
+#     return n
+#
+# s=ll()
+#单利模式
+class Mydata:
+    _log=None
+    def __new__(cls, *args, **kwargs):
+        cls._log=object.__new__(cls)
+        return cls
+ll=Mydata()
+ls=Mydata()
+print(id(ll))
+print(id(ls))
+
